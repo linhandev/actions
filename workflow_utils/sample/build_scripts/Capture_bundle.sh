a4cbe412f171b5fb40298e284a694a65e8f2a611
linhandev <hanlin21@huawei.com>
2025-07-08 23:33:06 +0800

    config
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- workflow_utils/sample/build_scripts/Capture_bundle.sh
+++ workflow_utils/sample/build_scripts/Capture_bundle.sh
@@ -4,10 +4,10 @@ cd ReactProject
 npm i ${HARMONY_PATH} --save-dev ${CLI_PATH}
 cat package.json
 npm run dev
+npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ../NativeProject/build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 cd -
 
 cd NativeProject
-npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 ohpm i
 hvigorw --sync -p product=default --analyze=false --parallel --incremental --no-daemon --debug
 hvigorw --mode module -p module=entry@default -p product=default -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=false --parallel --incremental --no-daemon --debug
