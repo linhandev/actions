ada25c335c341c9b5fbf2cef8eeee176a5341d2d
linhandev <hanlin21@huawei.com>
2025-07-08 21:38:18 +0800

    artifact
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- workflow_utils/sample/build_scripts/Capture_bundle.sh
+++ workflow_utils/sample/build_scripts/Capture_bundle.sh
@@ -12,4 +12,6 @@ npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts build-prof
 ohpm i
 hvigorw --sync -p product=default --analyze=false --parallel --incremental --no-daemon --debug
 hvigorw --mode module -p module=entry@default -p product=default -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=false --parallel --incremental --no-daemon --debug
+cd -
+
 echo "build succeed"
