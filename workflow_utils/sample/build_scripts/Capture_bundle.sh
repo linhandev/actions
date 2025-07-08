256a1846acb34488221b2a0fe46e6a76828b913c
linhandev <hanlin21@huawei.com>
2025-07-08 22:16:22 +0800

    fix
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- workflow_utils/sample/build_scripts/Capture_bundle.sh
+++ workflow_utils/sample/build_scripts/Capture_bundle.sh
@@ -7,7 +7,7 @@ npm run dev
 cd -
 
 cd NativeProject
-npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts build-profile.json5 com.example.capturebundle '$SIGNING_CONFIG_MAP'
+npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 ohpm i
 hvigorw --sync -p product=default --analyze=false --parallel --incremental --no-daemon --debug
 hvigorw --mode module -p module=entry@default -p product=default -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=false --parallel --incremental --no-daemon --debug
