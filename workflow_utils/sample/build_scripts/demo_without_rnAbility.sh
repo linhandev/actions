a336bae5805297a5e40a737d71831c1320caf4e4
linhandev <hanlin21@huawei.com>
2025-07-09 17:54:28 +0800

    demo_without_rnAbility
    Signed-off-by: linhandev <hanlin21@huawei.com>

---
+++ workflow_utils/sample/build_scripts/demo_without_rnAbility.sh
@@ -0,0 +1,18 @@
+set -ex
+
+cd RNProject/MainProject
+npm i ${HARMONY_PATH} --save-dev ${CLI_PATH}
+cat package.json
+npm run dev
+cd -
+
+npm install @tsconfig/recommended json5
+npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ./NativeProject/build-profile.json5 com.example.rnability "$SIGNING_CONFIG_MAP"
+
+cd NativeProject
+ohpm i
+hvigorw --sync -p product=default --analyze=false --parallel --incremental --no-daemon --debug
+hvigorw --mode module -p module=entry@default -p product=default -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=false --parallel --incremental --no-daemon --debug
+cd -
+
+echo "build succeed"
