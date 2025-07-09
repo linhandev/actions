03f91a530c3b387504e8d02c8089bb08d0e9c05f
linhandev <hanlin21@huawei.com>
2025-07-09 09:33:09 +0800

    npm i
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- workflow_utils/sample/build_scripts/Capture_bundle.sh
+++ workflow_utils/sample/build_scripts/Capture_bundle.sh
@@ -4,8 +4,10 @@ cd ReactProject
 npm i ${HARMONY_PATH} --save-dev ${CLI_PATH}
 cat package.json
 npm run dev
-npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ../NativeProject/build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 cd -
+
+npm install @tsconfig/recommended
+npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ../NativeProject/build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 
 cd NativeProject
 ohpm i
