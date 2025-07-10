6edfe11e9550d773e90254d981af8e560daf199f
linhandev <hanlin21@huawei.com>
2025-07-10 09:25:44 +0800

    fix
    Signed-off-by: linhandev <hanlin21@huawei.com>

--- workflow_utils/sample/build_scripts/Capture_bundle.sh
+++ workflow_utils/sample/build_scripts/Capture_bundle.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-source "$(dirname "$0")/common.sh"
 npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ./NativeProject/build-profile.json5 com.example.capturebundle "$SIGNING_CONFIG_MAP"
 
 cd ReactProject
