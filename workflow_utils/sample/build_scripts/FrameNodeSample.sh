c9b869a373bfa555fd3ab427bb52aebc9ea4e270
linhandev <hanlin21@huawei.com>
2025-07-10 12:10:33 +0800

    FrameNodeSample
    Signed-off-by: linhandev <hanlin21@huawei.com>

---
+++ workflow_utils/sample/build_scripts/FrameNodeSample.sh
@@ -0,0 +1,14 @@
+npx ts-node $GITHUB_WORKSPACE/workflow_utils/update_signing_config.ts ./FrameNodeSampleApp/build-profile.json5 com.example.framenode "$SIGNING_CONFIG_MAP"
+
+cd GenerateBundle
+install_dependencies
+npm run dev
+cd -
+
+cd FrameNodeSampleApp
+ohpm i
+hvigorw --sync -p product=default --analyze=false --parallel --incremental --no-daemon --debug
+hvigorw --mode module -p module=entry@default -p product=default -p buildMode=debug -p requiredDeviceType=phone assembleHap --analyze=false --parallel --incremental --no-daemon --debug
+cd -
+
+echo "build succeed"
